ae76e1d17ac691acbe20089504ac1af6aebab7ce
Greenfire44Official <48894984+Greenfire44Official@users.noreply.github.com>
2025-08-21 22:05:10 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rust extension for check50
+An extension for check50 to add support for rust. Pretty self explanatory.
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    author="Arturo Brandt",
+    author_email="arturobrandt@duck.com",
+    classifiers=[
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+        "Topic :: Utilities"
+    ],
+    description="This is check50_rs, a rust extension for check50.",
+    license="GPLv3",
+    install_requires=["python-bond>=1.4,<2", "check50>=3,<4"],
+    keywords=["check50_rs", "rust"],
+    name="check50_rs",
+    python_requires=">= 3.6",
+    url="https://github.com/ArturoBrandt/check50_rs",
+    version="0.0.1",
+    include_package_data=True
+)
