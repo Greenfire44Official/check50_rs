a9f901a3cd0dba27ebfc7e77334a22bc538eb746
Greenfire44Official <48894984+Greenfire44Official@users.noreply.github.com>
2025-08-21 23:09:14 -0600

    check50_rs.compile based on check50.c.compile

--- check50_rs.py
+++ check50_rs.py
@@ -1,1 +1,79 @@
-# Humble beginnings
+"""
+Based on check50's c extension https://github.com/cs50/check50/blob/main/check50/c.py
+"""
+
+# import contextlib
+import re
+from pathlib import Path
+from check50 import run, log, Failure
+
+#: Default compiler for :func:`check50_rs.compile`
+CC = "cargo"
+
+#: Default CFLAGS for :func:`check50_rs.compile`
+CFLAGS = {}
+
+
+def compile(*files, exe_name=None, cc=CC, max_log_lines=50, **cflags):
+    """
+    Compile Rust source files.
+
+    :param files: filenames to be compiled
+    :param exe_name: name of resulting executable
+    :param cc: compiler to use (:data:`check50_rs.CC` by default)
+    :param cflags: additional flags to pass to the compiler
+    :raises check50.Failure: if compilation failed (i.e., if the compiler returns a non-zero exit status).
+    :raises RuntimeError: if no filenames are specified
+
+    If ``exe_name`` is None, :func:`check50_rs.compile` will default to the first
+    file specified sans the ``.c`` extension::
+
+
+        check50_rs.compile("foo.rs", "bar.rs") # clang foo.rs bar.rs -o foo -std=c11 -ggdb -lm
+
+    Additional CFLAGS may be passed as keyword arguments like so::
+
+        check50_rs.compile("foo.rs", "bar.rs", lcs50=True) # clang foo.rs bar.rs -o foo -std=c11 -ggdb -lm -lcs50
+
+    In the same vein, the default CFLAGS may be overridden via keyword arguments::
+
+        check50_rs.compile("foo.rs", "bar.rs", std="c99", lm=False) # clang foo.rs bar.rs -o foo -std=c99 -ggdb
+    """
+
+    if not files:
+        raise RuntimeError(_("compile requires at least one file"))  # type: ignore
+
+    if exe_name is None and files[0].endswith(".rs"):
+        exe_name = Path(files[0]).stem
+
+    files = " ".join(files)
+
+    flags = CFLAGS.copy()
+    flags.update(cflags)
+    flags = " ".join(
+        (f"-{flag}" + (f"={value}" if value is not True else "")).replace("_", "-")
+        for flag, value in flags.items()
+        if value
+    )
+
+    out_flag = f" -o {exe_name} " if exe_name is not None else " "
+
+    if CC == "cargo":
+        process = run(f"{cc} build{flags}")
+    else:
+        process = run(f"{cc} {files}{out_flag}{flags}")
+
+    # Strip out ANSI codes
+    stdout = re.sub(r"\x1B\[[0-?]*[ -/]*[@-~]", "", process.stdout())  # type: ignore
+
+    # Log max_log_lines lines of output in case compilation fails
+    if process.exitcode != 0:
+        lines = stdout.splitlines()
+
+        if len(lines) > max_log_lines:
+            lines = lines[: max_log_lines // 2] + lines[-(max_log_lines // 2) :]
+
+        for line in lines:
+            log(line)
+
+        raise Failure("code failed to compile")
